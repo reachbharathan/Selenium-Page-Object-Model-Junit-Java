f7a6b5cce1c8a7478d7f7275eb9fb720f3ec4478
Bharathan Kumaran <reachbharathan@gmail.com>
2017-01-05 13:31:31 +0530

    Adding Testrunner for single point execution

---
+++ src/test/java/TestRunner.java
@@ -0,0 +1,17 @@
+import org.junit.runner.JUnitCore;
+
+/**
+ * Package name PACKAGE_NAME
+ * Created by Bharathan on 02/01/17.
+ * Project Name AccountDemoQAWorkshop
+ */
+public class TestRunner {
+  public static JUnitCore runner;
+
+  public static void main(String[] args) {
+    runner = new JUnitCore();
+    runner.run(SmokeSuite.class);
+    runner.run(RegressionSuite.class);
+  }
+
+}
